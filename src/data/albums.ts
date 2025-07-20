ec1ddf31be7dfbe62d2c2d3b311d445c0779be40
Chad <chad.farrow@gmail.com>
2025-07-19 21:13:52 -0400

    Expand album collection to match original site with 27 albums organized by categories

--- src/data/albums.ts
+++ src/data/albums.ts
@@ -7,7 +7,8 @@ export interface Album {
   description?: string;
 }
 
-export const FEATURED_ALBUMS: Album[] = [
+// The Doerfels Albums
+export const DOERFELS_ALBUMS: Album[] = [
   {
     id: 'bloodshot-lies',
     title: 'Bloodshot Lies - The Album',
@@ -17,12 +18,88 @@ export const FEATURED_ALBUMS: Album[] = [
     description: 'A powerful collection of rock anthems exploring themes of deception and resilience.'
   },
   {
+    id: 'think-ep',
+    title: 'Think EP',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/think-ep.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/think-ep.xml',
+    description: 'A contemplative EP that challenges conventional thinking through melodic storytelling.'
+  },
+  {
+    id: 'music-from-the-doerfelverse',
+    title: 'Music From The Doerfel-Verse',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/carol-of-the-bells.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/music-from-the-doerfelverse.xml',
+    description: 'A diverse collection spanning the musical universe of The Doerfels.'
+  },
+  {
+    id: 'ben-doerfel',
+    title: 'Ben Doerfel',
+    artist: 'Ben Doerfel',
+    artwork: 'https://www.doerfelverse.com/art/ben-doerfel-red.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/ben-doerfel.xml',
+    description: 'Solo work from Ben Doerfel'
+  },
+  {
+    id: 'into-the-doerfelverse',
+    title: 'Into the Doerfelverse',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/v4v-music-podcast.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/intothedoerfelverse.xml',
+    description: 'Journey into the Doerfelverse'
+  },
+  {
+    id: 'kurtisdrums-v1',
+    title: 'Kurtisdrums V1',
+    artist: 'Kurtis',
+    artwork: 'https://www.doerfelverse.com/art/kurtisdrums.png',
+    feedUrl: 'https://www.sirtjthewrathful.com/wp-content/uploads/2023/08/Kurtisdrums-V1.xml',
+    description: 'Drum compositions by Kurtis'
+  },
+  {
+    id: 'nostalgic',
+    title: 'Nostalgic',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/nostalgic.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/nostalgic.xml',
+    description: 'Nostalgic memories in musical form'
+  },
+  {
+    id: 'citybeach',
+    title: 'CityBeach',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/citybeach.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/citybeach.xml',
+    description: 'Urban beach vibes and summer sounds'
+  },
+  {
+    id: 'wrath-of-banjo',
+    title: 'Wrath of Banjo',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/wrath-of-banjo.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/wrath-of-banjo.xml',
+    description: 'Banjo-driven melodies with attitude'
+  },
+  {
+    id: 'ring-that-bell',
+    title: 'Ring That Bell',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/ring-that-bell.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/ring-that-bell.xml',
+    description: 'Bell-inspired musical compositions'
+  }
+];
+
+// Producers Picks Albums
+export const PRODUCERS_PICKS_ALBUMS: Album[] = [
+  {
     id: 'heycitizen-experience',
     title: 'The HeyCitizen Experience',
     artist: 'HeyCitizen',
-    artwork: 'https://files.heycitizen.xyz/Songs/Albums/HeyCitizen-Experience/HeyCitizen-Experience.png',
-    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/HeyCitizen-Experience/HeyCitizen-Experience.xml',
-    description: 'An eclectic journey through electronic soundscapes and experimental beats.'
+    artwork: 'https://files.heycitizen.xyz/Songs/Albums/The-Heycitizen-Experience/The-HEYCitizen-Experience-3000x3000.jpg',
+    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/The-Heycitizen-Experience/the heycitizen experience.xml',
+    description: 'An experimental journey through electronic soundscapes and indie vibes.'
   },
   {
     id: 'makin-beans',
@@ -31,30 +108,6 @@ export const FEATURED_ALBUMS: Album[] = [
     artwork: 'https://ableandthewolf.com/static/media/01_MakinBeans.6dfb9c8e18b0f28adf4d.jpg',
     feedUrl: 'https://ableandthewolf.com/static/media/feed.xml',
     description: 'Intimate acoustic sessions that invite listeners to pause and reflect.'
-  },
-  {
-    id: 'spectral-hiding',
-    title: 'Spectral Hiding',
-    artist: 'Bitpunk.fm',
-    artwork: 'https://files.bitpunk.fm/spectral_hiding.png',
-    feedUrl: 'https://zine.bitpunk.fm/feeds/spectral-hiding.xml',
-    description: 'Ethereal electronic compositions that blur the line between digital and organic.'
-  },
-  {
-    id: 'polar-embrace',
-    title: 'Polar Embrace',
-    artist: 'The Satellite Skirmish',
-    artwork: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/polarembrace/art/Polar-Embrace-Feed-art-hires.gif',
-    feedUrl: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/polarembrace/rss/videofeed/feed.xml',
-    description: 'Ambient soundscapes inspired by arctic solitude and cosmic wonder.'
-  },
-  {
-    id: 'autumn-rust',
-    title: 'Autumn Rust',
-    artist: 'The Satellite Skirmish',
-    artwork: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/autumnrust/art/Autumn-Rust-Feed-Art.gif',
-    feedUrl: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/autumnrust/mp3s/album_feed/feed.xml',
-    description: 'Melancholic melodies capturing the beauty of seasonal transformation.'
   },
   {
     id: 'lofi-experience',
@@ -81,6 +134,14 @@ export const FEATURED_ALBUMS: Album[] = [
     description: 'Haunting melodies exploring the darker corners of the human experience.'
   },
   {
+    id: 'pony-up-daddy',
+    title: 'Pony Up Daddy',
+    artist: '$2 Holla',
+    artwork: 'https://f4.bcbits.com/img/a1480089316_16.jpg',
+    feedUrl: 'https://music.behindthesch3m3s.com/wp-content/uploads/Mike_Epting/$2Holla/pony%20up%20daddy.xml',
+    description: 'High-energy beats and clever wordplay that demand attention.'
+  },
+  {
     id: 'empty-passenger-seat',
     title: 'Empty Passenger Seat',
     artist: 'Joe Martin',
@@ -89,19 +150,94 @@ export const FEATURED_ALBUMS: Album[] = [
     description: 'Indie rock journeys through solitude and self-discovery.'
   },
   {
-    id: 'ben-doerfel',
-    title: 'Ben Doerfel',
-    artist: 'Ben Doerfel',
-    artwork: 'https://www.doerfelverse.com/art/ben-doerfel-red.png',
-    feedUrl: 'https://www.doerfelverse.com/feeds/ben-doerfel.xml',
-    description: 'Solo work from Ben Doerfel'
+    id: 'aged-friends-old-whiskey',
+    title: 'Aged Friends & Old Whiskey',
+    artist: 'Various Artists',
+    artwork: 'https://files.heycitizen.xyz/Songs/Albums/Aged-Friends-Old-Whiskey/Aged-Friends-Old-Whiskey.png',
+    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/Aged-Friends-Old-Whiskey/aged-friends-old-whiskey.xml',
+    description: 'A collection celebrating friendship and good times.'
   },
   {
-    id: 'into-the-doerfelverse',
-    title: 'Into the Doerfelverse',
-    artist: 'The Doerfels',
-    artwork: 'https://www.doerfelverse.com/art/v4v-music-podcast.png',
-    feedUrl: 'https://www.doerfelverse.com/feeds/intothedoerfelverse.xml',
-    description: 'Journey into the Doerfelverse'
+    id: 'cosmodrome',
+    title: 'Cosmodrome',
+    artist: 'Various Artists',
+    artwork: 'https://files.heycitizen.xyz/Songs/Albums/Cosmodrome/Cosmodrome.png',
+    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/Cosmodrome/cosmodrome.xml',
+    description: 'Space-themed musical journey.'
+  },
+  {
+    id: 'east-to-west',
+    title: 'East To West',
+    artist: 'Various Artists',
+    artwork: 'https://files.heycitizen.xyz/Songs/Albums/East-To-West/East-To-West.png',
+    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/East-To-West/east-to-west.xml',
+    description: 'Cross-country musical adventure.'
+  },
+  {
+    id: 'tripodacus',
+    title: 'Tripodacus',
+    artist: 'Various Artists',
+    artwork: 'https://files.heycitizen.xyz/Songs/Albums/Tripodacus/Tripodacus.png',
+    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/Tripodacus/tripodacus.xml',
+    description: 'Experimental three-part musical composition.'
+  },
+  {
+    id: 'pilot',
+    title: 'Pilot',
+    artist: 'Various Artists',
+    artwork: 'https://files.heycitizen.xyz/Songs/Albums/Pilot/Pilot.png',
+    feedUrl: 'https://files.heycitizen.xyz/Songs/Albums/Pilot/pilot.xml',
+    description: 'Taking flight with musical exploration.'
+  },
+  {
+    id: 'spectral-hiding',
+    title: 'Spectral Hiding',
+    artist: 'Bitpunk.fm',
+    artwork: 'https://files.bitpunk.fm/spectral_hiding.png',
+    feedUrl: 'https://zine.bitpunk.fm/feeds/spectral-hiding.xml',
+    description: 'Ethereal electronic compositions that blur the line between digital and organic.'
   }
+];
+
+// Live Concerts Albums
+export const LIVE_CONCERTS_ALBUMS: Album[] = [
+  {
+    id: 'polar-embrace',
+    title: 'Polar Embrace',
+    artist: 'The Satellite Skirmish',
+    artwork: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/polarembrace/art/Polar-Embrace-Feed-art-hires.gif',
+    feedUrl: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/polarembrace/rss/videofeed/feed.xml',
+    description: 'Ambient soundscapes inspired by arctic solitude and cosmic wonder.'
+  },
+  {
+    id: 'autumn-rust',
+    title: 'Autumn Rust',
+    artist: 'The Satellite Skirmish',
+    artwork: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/autumnrust/art/Autumn-Rust-Feed-Art.gif',
+    feedUrl: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/autumnrust/mp3s/album_feed/feed.xml',
+    description: 'Melancholic melodies capturing the beauty of seasonal transformation.'
+  },
+  {
+    id: 'the-satellite-skirmish-album',
+    title: 'The Satellite Skirmish',
+    artist: 'Various Artists',
+    artwork: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/art/the%20satellite%20skirmish%20mku.gif',
+    feedUrl: 'https://music.behindthesch3m3s.com/wp-content/uploads/Sat_Skirmish/the_satellite_skirmish_album.xml',
+    description: 'A collaborative collection featuring diverse artists from the Satellite Skirmish collective.'
+  },
+  {
+    id: 'live-at-fickle-pickle',
+    title: 'Live at the Fickle Pickle - Into the Valquverse',
+    artist: 'The Doerfels',
+    artwork: 'https://www.doerfelverse.com/art/live-at-the-fickle-pickle.png',
+    feedUrl: 'https://www.doerfelverse.com/feeds/live-at-the-fickle-pickle.xml',
+    description: 'Live concert recording from the Fickle Pickle venue.'
+  }
+];
+
+// All albums combined for main gallery view
+export const FEATURED_ALBUMS: Album[] = [
+  ...DOERFELS_ALBUMS,
+  ...PRODUCERS_PICKS_ALBUMS,
+  ...LIVE_CONCERTS_ALBUMS
 ];
